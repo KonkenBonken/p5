760d40c3dab427ed72d2e9727413ab22463bf429
KonkenBonken <konrad05pettersson@gmail.com>
2022-05-28 00:08:28 +0200

    Displays flags

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
+function Color(decimal) {
+	const
+		r = (decimal & 0xff0000) >> 16,
+		g = (decimal & 0x00ff00) >> 8,
+		b = (decimal & 0x0000ff);
+
+	fill(r, g, b);
+	return [r, g, b]
+}
+
+function* Frames() {
+	while (true)
+		for (const i in Object.keys(flags)) {
+			const colors = Object.values(flags)[i];
+			for (var j in colors) {
+				Color(colors[j]);
+				rect(0, map(j, 0, colors.length, 0, height), width, map(j + 1, 0, colors.length, 0, height))
+			}
+			yield;
+		}
+}
+
+const frames = Frames();
+
 function setup() {
-	createCanvas(800, 800);
+	createCanvas(1200, 800);
+	frameRate(1);
+	noStroke(0)
+	console.log(flags)
 }
 
 function draw() {
 	background(0);
+	frames.next();
 }
