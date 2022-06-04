f9aa7e02e0e339df1e0ea30d9ee20c516b232af1
KonkenBonken <konrad05pettersson@gmail.com>
2022-06-04 23:32:31 +0200

    Added color

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
-const { sin, cos } = Math;
+const { sin, cos, max } = Math;
 
 class Slave {
-	constructor(masterX, masterY) {
+	constructor(masterX, masterY, hue) {
 		this.masterX = masterX;
 		this.masterY = masterY;
 		this.p = createVector(masterX.x, masterY.y);
 		this.path = [];
+		this.hue = hue;
 	}
 
 	draw() {
@@ -13,35 +14,36 @@ class Slave {
 			x = this.masterX.d.x + this.p.x,
 			y = this.masterY.d.y + this.p.y;
 
-		stroke(100);
+		stroke(this.hue, 30, 30);
 		strokeWeight(2);
 		for (var i = 1; i < this.path.length; i++)
 			line(...this.path[i], ...this.path[i - 1]);
 
-		stroke(255);
-		strokeWeight(5);
 		if (doPath) {
 			this.path.push([x, y]);
 			if (this.path.length >= 30)
 				this.path.shift();
 		}
+		stroke(this.hue, 100, 50);
+		strokeWeight(5);
 		point(x, y);
 	}
 }
 
 class Master {
-	constructor(x, y, speed) {
+	constructor(x, y, speed, hue) {
 		this.p = createVector(x, y);
 		this.d = createVector();
 		this.speed = speed;
+		this.hue = hue;
 	}
 
 	draw() {
-		stroke(100);
+		stroke(this.hue, 30, 30);
 		strokeWeight(2);
 		circle(this.p.x, this.p.y, d);
 
-		stroke(255);
+		stroke(this.hue, 100, 50);
 		strokeWeight(5);
 		var a = (frameCount / 150 * this.speed) % 360;
 		this.d.x = r * cos(a) + this.p.x;
@@ -66,19 +68,20 @@ function setup() {
 	createCanvas(size, size);
 	frameRate(100);
 	fill(0);
+	colorMode(HSL);
 
 	for (var i = 1; i < 10; i++) {
 		var x = tot / 2 + i * tot;
-		masters.x.push(new Master(x, tot / 2, i));
+		masters.x.push(new Master(x, tot / 2, i, map(i, 1, 9, 0, 192)));
 	}
 	for (var i = 1; i < 10; i++) {
 		var y = tot / 2 + i * tot;
-		masters.y.push(new Master(tot / 2, y, i));
+		masters.y.push(new Master(tot / 2, y, i, map(i, 1, 9, 0, 192)));
 	}
 
 	for (var masterX of masters.x)
 		for (var masterY of masters.y)
-			slaves.push(new Slave(masterX, masterY));
+			slaves.push(new Slave(masterX, masterY, map(max(masterX.speed, masterY.speed), 1, 9, 0, 192)));
 }
 
 let doPath = true;
