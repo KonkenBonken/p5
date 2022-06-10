70e85d3a3f3bc72db70f6b7f366a3371efeee40f
KonkenBonken <konrad05pettersson@gmail.com>
2022-06-11 00:57:29 +0200

    Initial commit

--- sketch.js
+++ sketch.js
@@ -1,10 +1,29 @@
+const { sin, cos, tan } = Math;
 const
 	size = window.innerHeight;
+let input, button, func;
+
+function setFunc() {
+	func = eval('x=>' + input.value());
+	redraw();
+}
 
 function setup() {
 	createCanvas(size, size);
+	noLoop();
+	createSpan('y = ');
+	input = createInput(random(['x*2', 'x-400', 'sin(x)+500', '300*sin(x/50)+500']));
+	button = createButton('Set');
+	button.mousePressed(setFunc);
+	setFunc();
 }
 
 function draw() {
 	background(0);
+	stroke(255);
+	let last = func(0);
+	for (let x = 1; x < width; x++) {
+		line(x - 1, last, x, func(x));
+		last = func(x);
+	}
 }
